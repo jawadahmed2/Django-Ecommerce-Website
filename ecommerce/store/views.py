28f9a1448b3dc2cfcad5a7c9722656771004a6a8
Jawad Ahmed <jawad.kohat2002@gmail.com>
2022-08-16 15:19:50 +0500

    update the views

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Make function to operate Store Functionality
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store/store.html',context)
 
 
