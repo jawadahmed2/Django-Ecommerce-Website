b7fcb8b0ce5b66b22b8df637b92ee1343315bd89
Jawad Ahmed <jawad.kohat2002@gmail.com>
2022-08-17 14:50:17 +0500

    complete the checkout

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Make function to operate Store Functionality
 
@@ -80,6 +81,31 @@ def updateItem(request):
 
     return JsonResponse('Item was added', safe=False)
 
-def processOrder(request):
-    return JsonResponse('Payment subbmitted..', safe= False)
 
+def processOrder(request):  # sourcery skip: extract-method
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode']
+
+
+            )
+
+    else:
+        print('User is not logged in ..')
+    return JsonResponse('Payment subbmitted..', safe=False)
